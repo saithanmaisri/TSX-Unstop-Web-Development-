b9198f715f625f08db7eb4e74b0603fc4b492567
saithanmaisri <saithanmai2005@gmail.com>
2025-05-14 19:15:12 +0530

    Add files via upload

---
+++ HTML CSS Week 1 tsx/script.js
@@ -0,0 +1,7 @@
+// Log message when page loads
+console.log("Page has loaded successfully!");
+
+// Show an alert when the button is clicked
+document.getElementById("clickBtn").addEventListener("click", function() {
+  alert("Thanks for clicking!");
+});
