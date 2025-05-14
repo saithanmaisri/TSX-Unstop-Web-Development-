c5e951bb58a0d7f5e4572adafa8b29aed077ef67
saithanmaisri <saithanmai2005@gmail.com>
2025-05-14 19:15:58 +0530

    Delete topic 1.py

--- topic 1.py
+++
@@ -1,9 +0,0 @@
-# This script prints a greeting message with name
-print("Hello, my name is Sai Thanmaisri!")
-print("Nice to meet you!")
-# Store the name in a variable
-name = "Sai Thanmaisri"
-# Displaying the greeting message using the name
-print("Hello, my name is", name)
-# Print a second message
-print("Welcome to Python programming!")
