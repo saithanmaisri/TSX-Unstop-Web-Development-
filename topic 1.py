d556f4858c2d4b79e53f777985976b163eee3993
saithanmaisri <saithanmai2005@gmail.com>
2025-05-14 18:43:45 +0530

    Add files via upload

---
+++ topic 1.py
@@ -0,0 +1,9 @@
+# This script prints a greeting message with name
+print("Hello, my name is Sai Thanmaisri!")
+print("Nice to meet you!")
+# Store the name in a variable
+name = "Sai Thanmaisri"
+# Displaying the greeting message using the name
+print("Hello, my name is", name)
+# Print a second message
+print("Welcome to Python programming!")
